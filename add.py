d57f091a4110d7a68427059dab63966d068aa878
Your Name <your@email.com>
2026-05-03 09:20:17 +0530

    third commit

--- add.py
+++ add.py
@@ -1,7 +1,7 @@
 a = int(input("Enter a value"))
 b= int(input("Enter a value"))
 if a or b <=0:
-    print("Negative value")
+    print("Negative value , Please enter Postive value only")
 else:
     c=a+b
     print("The sum is = ",c)
